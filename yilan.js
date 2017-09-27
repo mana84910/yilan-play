9f530384c8ec741537eec10373c117df1c3eecf0
Jessie <mana84910@gmail.com>
2017-09-27 10:10:15 +0800

    add slider

--- yilan.js
+++ yilan.js
@@ -2,5 +2,5 @@
 $(".menu_icon").click(function(){
 	$(this).toggleClass("active");
 	$(".nav_menu").toggleClass("nav_menu_active");
-	$(".menu_list_container").toggleClass("menu_list_container_active");
+	$(".menu_list").toggleClass("menu_list_active");
 });
